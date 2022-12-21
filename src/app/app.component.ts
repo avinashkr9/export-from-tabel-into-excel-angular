e7c100799aee1c49896567c26f9b559d0e9f5186
avinash_kumar_zeno <avinash.kumar@zeno.health>
2022-12-21 19:33:44 +0530

    Export excel added..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title = 'Angular project';
+  fileName= 'ExcelSheet.xlsx';
+
+  public userList = [
+
+    {
+    
+    "id": 1,
+    
+    "name": "Avinash Kumar",
+    
+    "username": "Abhi",
+    
+    "email": "demo@gmail.com"
+    
+    },
+    {
+      "id": 2,
+      
+      "name": "Avinash Singh",
+      
+      "username": "ak",
+      
+      "email": "demo@gmail.com"
+      
+      },
+      {
+    
+        "id": 3,
+        
+        "name": "Aakash Bhardwaj",
+        
+        "username": "akash",
+        
+        "email": "demo@gmail.com"
+        
+      },
+
+
+    
+    ]
+
+    exportexcel(): void
+    {
+      /* pass here the table id */
+      let element = document.getElementById('excel-table');
+      const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+   
+      /* generate workbook and add the worksheet */
+      const wb: XLSX.WorkBook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+   
+      /* save to file */  
+      XLSX.writeFile(wb, this.fileName);
+   
+    }
+
 }
